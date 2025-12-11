92b3861eb6450d7a4ea7cff54269b7b9b08a0802
Paulo71092 <paulovilar710@gmail.com>
2025-12-11 23:43:53 +0000

    branch creation

---
+++ src/main/java/webServer/webSock/webSocket.java
@@ -0,0 +1,24 @@
+package webServer.webSock;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class webSocket implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Endpoint de conexão inicial do cliente, conforme seu código
+        // O cliente se conectará a 'ws://servidor/my-websocket'
+        registry.addEndpoint("/my-websocket").withSockJS();
+    }
+}
